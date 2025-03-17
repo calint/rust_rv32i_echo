2a391b271e7369a6e0db6a50880fe3a5a3a96e85
calint <calin.tenitchi@gmail.com>
2025-03-17 15:17:38 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,13 @@ use core::arch::global_asm;
 use core::panic::PanicInfo;
 use core::ptr::{read_volatile, write_volatile};
 
-mod constants; // Declare the constants module
-use constants::*; // Bring constants into scope
+mod constants;
+use constants::*;
 
-// Hardware register definitions
-#[inline(always)]
-unsafe fn uart_out() -> *mut i32 {
-    UART_OUT_ADDR as *mut i32
-}
-
-#[inline(always)]
-unsafe fn uart_in() -> *mut i32 {
-    UART_IN_ADDR as *mut i32
-}
-
-fn uart_read_blocking() -> i8 {
+fn uart_read_char() -> i8 {
     loop {
         unsafe {
-            let input = read_volatile(uart_in());
+            let input = read_volatile(UART_IN_ADDR as *const i32);
             if input == -1 {
                 continue;
             }
@@ -31,20 +20,20 @@ fn uart_read_blocking() -> i8 {
     }
 }
 
-fn uart_write_blocking(ch: i8) {
+fn uart_send_char(ch: i8) {
     unsafe {
-        while read_volatile(uart_out()) != -1 {}
-        write_volatile(uart_out(), ch as i32);
+        while read_volatile(UART_OUT_ADDR as *const i32) != -1 {}
+        write_volatile(UART_OUT_ADDR as *mut i32, ch as i32);
     }
 }
 
 global_asm!(include_str!("startup.s"));
 
-// Entry point after stack setup
+// Entry point after 'startup.s'
 #[no_mangle]
 pub extern "C" fn run() -> ! {
     loop {
-        uart_write_blocking(uart_read_blocking());
+        uart_send_char(uart_read_char());
     }
 }
 
