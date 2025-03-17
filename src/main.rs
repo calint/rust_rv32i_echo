13b69f9120b4f66a207037e9e71871d385900bba
calint <calin.tenitchi@gmail.com>
2025-03-17 16:04:32 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ use core::panic::PanicInfo;
 use core::ptr::{read_volatile, write_volatile};
 
 mod constants; // FPGA addresses
+use constants::*;
 
 // API
 fn uart_read_char() -> u8 {
     loop {
         unsafe {
-            let input = read_volatile(constants::UART_IN_ADDR as *const i32);
+            let input = read_volatile(UART_IN_ADDR as *const i32);
             if input == -1 {
                 continue;
             }
@@ -22,8 +23,8 @@ fn uart_read_char() -> u8 {
 
 fn uart_send_char(ch: u8) {
     unsafe {
-        while read_volatile(constants::UART_OUT_ADDR as *const i32) != -1 {}
-        write_volatile(constants::UART_OUT_ADDR as *mut i32, ch as i32);
+        while read_volatile(UART_OUT_ADDR as *const i32) != -1 {}
+        write_volatile(UART_OUT_ADDR as *mut i32, ch as i32);
     }
 }
 
@@ -31,9 +32,18 @@ fn uart_send_cstr(cstr: *const u8) {
     unsafe {
         let mut ptr = cstr;
         while *ptr != 0 {
-            while read_volatile(constants::UART_OUT_ADDR as *const i32) != -1 {}
-            write_volatile(constants::UART_OUT_ADDR as *mut i32, *ptr as i32);
+            while read_volatile(UART_OUT_ADDR as *const i32) != -1 {}
+            write_volatile(UART_OUT_ADDR as *mut i32, *ptr as i32);
             ptr = ptr.offset(1);
+        }
+    }
+}
+
+fn uart_send_str(str: &[u8]) {
+    unsafe {
+        for &byte in str {
+            while read_volatile(UART_OUT_ADDR as *const i32) != -1 {}
+            write_volatile(UART_OUT_ADDR as *mut i32, byte as i32);
         }
     }
 }
@@ -43,7 +53,8 @@ global_asm!(include_str!("startup.s"));
 
 #[no_mangle]
 pub extern "C" fn run() -> ! {
-    uart_send_cstr(b"hello world. echo:\r\n\0".as_ptr());
+    // uart_send_cstr("hello world.\0".as_ptr());
+    uart_send_str(b"hello world. echo below:\r\n");
     loop {
         uart_send_char(uart_read_char());
     }
