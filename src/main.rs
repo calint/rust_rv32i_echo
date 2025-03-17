7131564d7ef1dabfb0cf9937ccb639756262cb11
calint <calin.tenitchi@gmail.com>
2025-03-17 15:57:29 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,23 @@ fn uart_send_char(ch: u8) {
     }
 }
 
+fn uart_send_cstr(cstr: *const u8) {
+    unsafe {
+        let mut ptr = cstr;
+        while *ptr != 0 {
+            while read_volatile(constants::UART_OUT_ADDR as *const i32) != -1 {}
+            write_volatile(constants::UART_OUT_ADDR as *mut i32, *ptr as i32);
+            ptr = ptr.offset(1);
+        }
+    }
+}
+
 // setup stack and jump to 'run()'
 global_asm!(include_str!("startup.s"));
 
 #[no_mangle]
 pub extern "C" fn run() -> ! {
+    uart_send_cstr(b"hello world. echo:\r\n\0".as_ptr());
     loop {
         uart_send_char(uart_read_char());
     }
